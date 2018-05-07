01e65d5b37258e0657c736fc03658083db7e81a8
marikoja <Marikoanderson@gmail.com>
2018-05-07 14:22:56 -0700

    adds tests for customer model and validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,3 @@
 class Customer < ApplicationRecord
+  validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,51 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { Customer.new(name: "Shelley Rocha",
+  registered_at: DateTime.new(2015, 4, 29),
+  address: "Ap #292-5216 Ipsum Rd.",
+  city: "Hillsboro",
+  state: "OR",
+  postal_code: "24309",
+  phone: "(322) 510-8695") }
 
   it "must be valid" do
     value(customer).must_be :valid?
   end
+
+  it "must be invalid with missing name" do
+    customer.name = nil
+    value(customer).wont_be :valid?
+  end
+
+  it "must be invalid with missing registered_at" do
+    customer.registered_at = nil
+    value(customer).wont_be :valid?
+  end
+
+  it "must be invalid with missing address" do
+    customer.address = nil
+    value(customer).wont_be :valid?
+  end
+
+  it "must be invalid with missing city" do
+    customer.city = nil
+    value(customer).wont_be :valid?
+  end
+
+  it "must be invalid with missing state" do
+    customer.state = nil
+    value(customer).wont_be :valid?
+  end
+
+  it "must be invalid with missing postal_code" do
+    customer.postal_code = nil
+    value(customer).wont_be :valid?
+  end
+
+  it "must be invalid with missing phone" do
+    customer.phone = nil
+    value(customer).wont_be :valid?
+  end
+
 end
