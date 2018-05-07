7b1b37cb86c4e444912107c981335a409c017c1d
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-07 15:46:08 -0700

    added nominal customer controller tests for the index action

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,36 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_index_url
-    value(response).must_be :success?
+  describe "index" do
+    it "is a real working route" do
+      get customers_url
+
+      must_respond_with :success
+    end
+
+    it "return json" do
+      get customers_url
+
+      response.header['Content-Type'].must_include "json"
+    end
+
+    it "returns an Array of all Customers" do
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with exactly the required fields" do
+      keys = %w(id movies_checked_out_count name phone postal_code registered_at)
+      get customers_url
+
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal keys
+      end
+    end
   end
 
 end
