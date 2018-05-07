4d32360c23417871a23a8a6a3921f1fe01afc1ab
marikoja <Marikoanderson@gmail.com>
2018-05-07 15:21:40 -0700

    adds movies_checked_out_count to customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone]), status: :ok
+    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count]), status: :ok
   end
 end
