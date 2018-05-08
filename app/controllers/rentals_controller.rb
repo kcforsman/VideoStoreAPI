e853e4cc7943039643df4087244911e88eba7749
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-08 15:05:25 -0700

    rentals_controller check_in code and error handling and testing

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,14 +5,31 @@ class RentalsController < ApplicationController
     movie = Movie.find_by(id: params[:movie_id])
 
     if customer && movie
-      Rental.create(customer: customer, movie: movie, checkout_date: Date.today, due_date: Date.today + 7.days)
-      render json: { status: 200}
+      rental = Rental.new(customer: customer, movie: movie, checkout_date: Date.today, due_date: Date.today + 7.days)
+      if rental.save
+        render json: { status: 200}
+      # else
+      #   render json: { ok: false, errors: rental.errors}, status: :bad_request
+      end
     else
       render json: { ok: false, errors: "Invalid movie or customer"}, status: 404
     end
   end
 
   def check_in
-
+    customer = Customer.find_by(id: params[:customer_id])
+    movie = Movie.find_by(id: params[:movie_id])
+    if customer && movie
+      rental = Rental.find_by(customer: customer, movie: movie, returned: false)
+      if rental
+        rental.returned = true
+        rental.save
+        render json: { status: 200}
+      else
+        render json: { ok: false, errors: "Invalid rental"}, status: 404
+      end
+    else
+      render json: { ok: false, errors: "Invalid movie or customer"}, status: 404
+    end
   end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 describe RentalsController do
   let(:customer) {customers(:shell)}
+  let(:customer2) {customers(:curr)}
   let(:movie) {movies(:women)}
+  let(:movie2) {movies(:savior)}
 
   describe 'check_out' do
     it "should get check-out" do
@@ -30,10 +32,64 @@ describe RentalsController do
       body["errors"].must_equal "Invalid movie or customer"
     end
   end
-  it "should get check-in" do
-    skip
-    get rentals_check-in_url
-    value(response).must_be :success?
+
+  describe "check-in" do
+    it "should get check-in" do
+      post check_in_url, params: { customer_id: customer2.id, movie_id: movie2.id }
+
+      value(response).must_be :success?
+    end
+
+    it "check-in should change returned status to true" do
+      rental = rentals(:one)
+
+      post check_in_url, params: { customer_id: customer2.id, movie_id: movie2.id }
+
+      rental = Rental.find_by(id: rental.id)
+      rental.returned.must_equal true
+    end
+
+    it "should render status not_found for invalid customer " do
+      id = customer.id
+      customer.destroy
+      post check_in_url, params: { customer_id: id, movie_id: movie2.id }
+      value(response).must_be :not_found?
+    end
+
+    it "should render status not_found for invalid movie " do
+      id = movie.id
+      movie.destroy
+      post check_in_url, params: { customer_id: customer2.id, movie_id: id }
+
+      value(response).must_be :not_found?
+      body = JSON.parse(response.body)
+      body.must_include "ok"
+      body["ok"].must_equal false
+      body.must_include "errors"
+      body["errors"].must_equal "Invalid movie or customer"
+    end
+
+    it "should render status not_found for rental that DNE" do
+      post check_in_url, params: { customer_id: customer.id, movie_id: movie.id }
+
+      value(response).must_be :not_found?
+      body = JSON.parse(response.body)
+      body.must_include "ok"
+      body["ok"].must_equal false
+      body.must_include "errors"
+      body["errors"].must_equal "Invalid rental"
+    end
+
+    it "should render status not_found for rental with true returned status" do
+      post check_in_url, params: { customer_id: customer2.id, movie_id: movies(:blacksmith).id }
+
+      value(response).must_be :not_found?
+      body = JSON.parse(response.body)
+      body.must_include "ok"
+      body["ok"].must_equal false
+      body.must_include "errors"
+      body["errors"].must_equal "Invalid rental"
+    end
   end
 
 end
