c931c6e1d71b756dd5078688331c21035525d172
marikoja <Marikoanderson@gmail.com>
2018-05-11 13:53:01 -0700

    rentals controller tests checkout inventory management

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -14,6 +14,7 @@ describe RentalsController do
 
     it "should render status not_found for invalid customer " do
       id = customer.id
+      customer.rentals.each {|rental| rental.destroy}
       customer.destroy
       post check_out_url, params: { customer_id: id, movie_id: movie.id }
       value(response).must_be :not_found?
@@ -30,6 +31,26 @@ describe RentalsController do
       body["ok"].must_equal false
       body.must_include "errors"
       body["errors"].must_equal "Invalid movie or customer"
+    end
+
+    it "should decrease decrease the available_inventory and increase the movies_checked_out_count by 1" do
+      post check_out_url, params: { customer_id: customer.id, movie_id: movie.id }
+
+      updated_cust = Customer.find_by(id: customer.id)
+      updated_cust.movies_checked_out_count.must_equal 2
+
+      updated_movie = Movie.find_by(id: movie.id)
+      updated_movie.available_inventory.must_equal 3
+    end
+
+    it "should not change the rental count, available_inventory or movies_checked_out_count if movie does not have available inventory" do
+      proc { post check_out_url, params: { customer_id: customer.id, movie_id: movie2.id} }.wont_change 'Rental.count'
+
+      updated_cust = Customer.find_by(id: customer.id)
+      updated_cust.movies_checked_out_count.must_equal 1
+
+      updated_movie = Movie.find_by(id: movie2.id)
+      updated_movie.available_inventory.must_equal 0
     end
   end
 
@@ -51,6 +72,7 @@ describe RentalsController do
 
     it "should render status not_found for invalid customer " do
       id = customer.id
+      customer.rentals.each {|rental| rental.destroy}
       customer.destroy
       post check_in_url, params: { customer_id: id, movie_id: movie2.id }
       value(response).must_be :not_found?
@@ -92,4 +114,24 @@ describe RentalsController do
     end
   end
 
+  describe "Overdue" do
+    it "can generate list of overdue rentals " do
+      get overdue_url
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      must_respond_with :success
+      body.length.must_equal 1
+    end
+
+    it "returns movies with exactly the required fields" do
+      # must be listed in alphabetical order
+      keys = %w(checkout_date customer_id due_date movie_id name postal_code title)
+      get overdue_url
+      body = JSON.parse(response.body)
+      body.each do |rental|
+        rental.keys.sort.must_equal keys
+      end
+    end
+  end
 end
